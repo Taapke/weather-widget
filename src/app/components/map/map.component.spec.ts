785d94cf77852f842ad8a2ed19d9fb44801803c2
Taapke <64285579+Taapke@users.noreply.github.com>
2024-02-04 14:44:16 +0100

    Unit test selecting location in map

--- src/app/components/map/map.component.spec.ts
+++ src/app/components/map/map.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MapComponent } from './map.component';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { LatLng, LeafletMouseEvent, circleMarker } from 'leaflet';
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -12,10 +13,9 @@ describe('MapComponent', () => {
     await TestBed.configureTestingModule({
       providers: [HttpClient, HttpHandler],
       declarations: [MapComponent],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
-    
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,5 +23,39 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call setMarker and OpenForecastDialog when location is present', () => {
+    const mouseEventMock = { latlng: new LatLng(50, 50) } as LeafletMouseEvent;
+
+    spyOn(component, 'setMarker');
+    spyOn(component, 'openForecastDialog');
+
+    component.selectLocation(mouseEventMock);
+
+    expect(component.setMarker).toHaveBeenCalledWith(mouseEventMock.latlng);
+    expect(component.openForecastDialog).toHaveBeenCalledWith(mouseEventMock.latlng);
+  });
+
+  it('should log error if location is not present', () => {
+    const mouseEventMock = {} as LeafletMouseEvent;
+
+    spyOn(console, 'error');
+
+    component.selectLocation(mouseEventMock);
+
+    expect(console.error).toHaveBeenCalledWith(
+      'Location information not found'
+    );
+  });
+
+  it('should add circle at selected location to map when setMarker is called', () => {
+    const location = new LatLng(50, 50);
+
+    component.setMarker(location);
+
+    expect(component.layers).toEqual([
+      circleMarker([location.lat, location.lng], { radius: 10 }),
+    ]);
   });
 });
