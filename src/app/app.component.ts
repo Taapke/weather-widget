fc285bbe1daf47395b42429892634506fe82d5a8
Taapke <64285579+Taapke@users.noreply.github.com>
2024-02-04 12:17:20 +0100

    Change title at homescreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'weather-widget';
+  title = 'World Wide Weather';
 
   constructor() {}
 }
